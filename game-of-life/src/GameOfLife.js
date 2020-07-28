8c09a8165421b148ef0c182461def651937b57f4
Ian <mus5002@aol.com>
2020-07-27 22:03:23 -0400

    board state added, working on rendering board

--- game-of-life/src/GameOfLife.js
+++ game-of-life/src/GameOfLife.js
@@ -3,22 +3,32 @@ import Cell from './Cell'
 import './GameOfLife.css'
 
 const GameOfLife = () => {
-    const [rows, setRows] = useState(25)
-    const [cols, setCols] = useState(25)
-    const [board, setBoard] = useState([])
+    const [rows, setRows] = useState(5)
+    const [cols, setCols] = useState(5)
+    const [board, setBoard] = useState({})
     const [cells, setCells] = useState([])
 
     const createGrid = () => {
-        for (let y = 0; y<rows; y++) {
-            setBoard(board[y] = [])
-            for (let x = 0; x<cols; x++) {
-                setBoard(board[y][x] = false)
-            }
+        setBoard([])
+        for (let i = 1; i<=(rows*cols); i++) {
+            setBoard(board[i] = false)
+        }
+        console.log(board)
+    }
+
+    const renderGrid = () => {
+        console.log(rows*cols)
+        for (let i = 0; i<(rows*cols); i++) {
+            console.log(i)
+            return setCells(cells[i] = <Cell key={i}/>)
         }
     }
     
     return <div>
-        <Cell />
+        <div className='board'>
+            {board[1] && renderGrid()}
+            {cells}
+        </div>
         <div className='controls'>
             <button onClick={() => createGrid()}>Create Board</button>
         </div>
