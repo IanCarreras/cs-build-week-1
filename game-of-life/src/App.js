ad922b835a5d3714d872ff4c24deaaae557316df
Ian <mus5002@aol.com>
2020-08-03 19:39:51 -0400

    added game rules

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <h1>Game of Life</h1>
+      <p>
+        Each box is a cell.  Each adjacent box is a neighbor.<br/>
+        Any live cell with fewer than two live neighbors dies, as if by underpopulation.<br/>
+        Any live cell with two or three live neighbors lives on to the next generation.<br/>
+        Any live cell with more than three live neighbors die, as if by overpopulation.<br/>
+        Any dead vell with exactly three live neighbors becomes a live cell, as if by reproduction.
+      </p>
       <GameOfLife />
     </div>
   );
